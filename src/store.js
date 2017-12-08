4d0280ca782477792aee405d80eb317865101eb1
Tom Gates <thomas.gates@shopify.com>
2017-12-08 09:31:42 -0500

    Store state in localstorage

--- src/store.js
+++ src/store.js
@@ -1,12 +1,16 @@
 import { createStore } from "redux";
 import reducer from 'reducers/index';
-
+import { saveState, loadState } from './localStorage';
 // Store
-const initialState = {};
+const initialState = loadState();
 const store = createStore(
   reducer,
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+})
 
 export default store;
